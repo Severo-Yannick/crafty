3fb213da023e5d5e638e4085e7fccf38b15c52f5
Severo-Yannick <severoyannick@gmail.com>
2023-10-01 10:27:45 +0200

    test: User cannot an empty post and more than 280 characters

--- src/post-message.usecase.ts
+++ src/post-message.usecase.ts
@@ -1,30 +1,34 @@
+export type Message = {
+  id: string;
+  author: string;
+  text: string;
+  publishedAt: Date;
+};
 export type PostMessageCommand = {
   id: string;
   text: string;
   author: string;
 };
-
-export type Message = {
-  id: string;
-  text: string;
-  author: string;
-  publishedAt: Date;
-};
-
 export interface MessageRepository {
   save(message: Message): void;
 }
-
 export interface DateProvider {
   getNow(): Date;
 }
+
+export class MessageTooLongError extends Error {}
+export class EmptyMessageError extends Error {}
 
 export class PostMessageUseCase {
   constructor(
     private readonly messageRepository: MessageRepository,
     private readonly dateProvider: DateProvider
-  ){};
+  ) {}
+
   handle(postMessageCommand: PostMessageCommand) {
+    if (postMessageCommand.text.length > 280) {
+      throw new MessageTooLongError();
+    }
     this.messageRepository.save({
       id: postMessageCommand.id,
       text: postMessageCommand.text,
--- src/tests/posting-message.spec.ts
+++ src/tests/posting-message.spec.ts
@@ -1,60 +1,96 @@
 import {
-  PostMessageUseCase,
-  PostMessageCommand,
+  DateProvider,
+  EmptyMessageError,
   Message,
   MessageRepository,
-  DateProvider,
+  MessageTooLongError,
+  PostMessageCommand,
+  PostMessageUseCase,
 } from "../post-message.usecase";
-
 describe("Feature: Posting a message", () => {
   describe("Rule: A message can contain a maximum of 280 characters", () => {
-    test("Alice can post a message on her timline", () => {
+    test("Alice can post a message on her timeline", () => {
       givenNowIs(new Date("2023-01-19T19:00:00.000Z"));
-
-      whenUserPostMessage({
+      whenUserPostsAmessage({
         id: "message-id",
-        text: "Hello World",
+        text: "Hello World 2",
         author: "Alice",
       });
-
-      thePostMessageShouldBe({
+      thenPostedMessageShouldBe({
         id: "message-id",
-        text: "Hello World",
         author: "Alice",
+        text: "Hello World 2",
         publishedAt: new Date("2023-01-19T19:00:00.000Z"),
       });
+    });
+
+    test("Alice cannot post a message with more than 280 characters", () => {
+      const textWithLengthOf281 =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mauris lacus, fringilla eu est vitae, varius viverra nisl. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus suscipit feugiat sollicitudin. Aliquam erat volutpat amet.";
+      givenNowIs(new Date("2023-01-19T19:00:00.000Z"));
+
+      whenUserPostsAmessage({
+        id: "message-id",
+        author: "Alice",
+        text: textWithLengthOf281,
+      });
+
+      thenErrorShouldBe(MessageTooLongError);
+    });
+  });
+
+  describe("Rule: A message cannot be empty", () => {
+    test("Alice cannot post an empty message", () => {
+      givenNowIs(new Date("2023-01-19T19:00:00.000Z"));
+
+      whenUserPostsAmessage({
+        id: "message-id",
+        author: "Alice",
+        text: "",
+      });
+
+      thenErrorShouldBe(EmptyMessageError);
     });
   });
 });
 
 let message: Message;
-
-function givenNowIs(_now: Date) {
-  dateProvider.now = _now;
-}
+let thrownError: Error;
 
 class InMemoryMessageRepository implements MessageRepository {
   save(msg: Message): void {
     message = msg;
   }
 }
-
 class StubDateProvider implements DateProvider {
   now: Date;
   getNow(): Date {
     return this.now;
   }
 }
-
 const messageRepository = new InMemoryMessageRepository();
 const dateProvider = new StubDateProvider();
-
-const postMessageUseCase = new PostMessageUseCase(messageRepository, dateProvider);
-
-function whenUserPostMessage(postMessageCommand: PostMessageCommand) {
-  postMessageUseCase.handle(postMessageCommand);
+const postMessageUseCase = new PostMessageUseCase(
+  messageRepository,
+  dateProvider
+);
+function givenNowIs(_now: Date) {
+  dateProvider.now = _now;
 }
 
-function thePostMessageShouldBe(expectedMessage: Message) {
+function whenUserPostsAmessage(postMessageCommand: PostMessageCommand) {
+  postMessageUseCase.handle(postMessageCommand);
+  try {
+    postMessageUseCase.handle(postMessageCommand);
+  } catch (err) {
+    thrownError = err;
+  }
+}
+
+function thenPostedMessageShouldBe(expectedMessage: Message) {
   expect(expectedMessage).toEqual(message);
+}
+
+function thenErrorShouldBe(expectedErrorClass: new () => Error) {
+  expect(thrownError).toBeInstanceOf(expectedErrorClass);
 }
